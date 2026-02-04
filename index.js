bcb809f74cab7a6d8f0b68ae0e8c50c09f0e398c
Surya <suryacsework@gmail.com>
2026-02-04 09:16:08 +0530

    chore: Updated Post request

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 const Product = require('./model/product.model');
+
 //Mongoose
 const mongoose = require('mongoose');
 const app = express();
+
+const username = process.env.DB_Username;
+const password = process.env.DB_Password;
+const dbName = process.env.DB_Name;
 
 app.use(express.json());
 
@@ -12,14 +18,14 @@ app.get('/', (req, res) => {
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body);
-        res.status(201).json(product);
+        res.status(200).json(product);
 
     }
     catch (error) {
         res.status(500).json({ message: error.message });
     }
 })
-mongoose.connect('mongodb+srv://suryacsework_db_user:2uwT2sDuwUVmGoGo@backendapi.wajtsvu.mongodb.net/?appName=BackendAPI').
+mongoose.connect(`mongodb+srv://${username}:${password}@backendapi.wajtsvu.mongodb.net/${dbName}?appName=BackendAPI`).
     then(() => {
         console.log('Connected to MongoDB');
         app.listen(3000, () => {
--- model/product.model.js
+++ model/product.model.js
@@ -21,7 +21,7 @@ const ProductSchema = new mongoose.Schema(
         }
     },
     {
-        timestamps: true;
+        timestamps: true,
     }
 
 )
